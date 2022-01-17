e3baa6215da219bf68295c55b2cab0e8ab551790
ST0PHoon <codingnb306@naver.com>
2022-01-17 17:32:24 +0900

    아이콘변경, 로그인 페이지 생성

--- learn-vuetify/src/App.vue
+++ learn-vuetify/src/App.vue
@@ -1,35 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    <v-app-bar-nav-icon @click="drawer = !drawer" />
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-spacer />
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
+          <v-list-item-title class="text-h6"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider />
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          :to="item.to" 
-        >
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -40,24 +24,25 @@
       </v-list>
     </v-navigation-drawer>
     <v-main>
-        <v-container fluid>
-          <router-view />
-        </v-container>
+      <v-container fluid>
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     drawer: false,
     items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
-          { title: 'Grid-System', icon: 'mdi-view-dashboard', to: '/grid-system' },
-          { title: 'Breakpoint', icon: 'mdi-view-dashboard', to: '/breakpoints' },
-          { title: 'Typography', icon: 'mdi-view-dashboard', to: '/typography' },
-        ],
+      { title: "Dashboard", icon: "mdi-view-dashboard", to: "/" },
+      { title: "Grid-System", icon: "mdi-border-all", to: "/grid-system" },
+      { title: "Breakpoint", icon: "mdi-cards-spade", to: "/breakpoints" },
+      { title: "Typography", icon: "mdi-alpha-a-box", to: "/typography" },
+      { title: "Login", icon: "mdi-account", to: "/login" },
+    ],
     right: null,
   }),
 };
--- learn-vuetify/src/router/index.js
+++ learn-vuetify/src/router/index.js
@@ -1,39 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 //페이지 추가
-import Dashboard from '@/views/Dashboard'
-import GridSystem from '@/views/GridSystem'
-import Breakpoints from '@/views/Breakpoints'
-import Typography from '@/views/Typography'
+import Dashboard from "@/views/Dashboard";
+import GridSystem from "@/views/GridSystem";
+import Breakpoints from "@/views/Breakpoints";
+import Typography from "@/views/Typography";
+import Login from "@/views/Login";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'Dashboard',
-        component: Dashboard
-    },
-    {
-        path: '/grid-system',
-        name: 'GridSystem',
-        component: GridSystem
-    },
-    {
-        path: '/breakpoints',
-        name: 'Breakpoints',
-        component: Breakpoints
-    },
-    {
-        path: '/typography',
-        name: 'Typography',
-        component: Typography
-    },
-]
+  {
+    path: "/",
+    name: "Dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/grid-system",
+    name: "GridSystem",
+    component: GridSystem,
+  },
+  {
+    path: "/breakpoints",
+    name: "Breakpoints",
+    component: Breakpoints,
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    component: Typography,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+];
 
 const router = new VueRouter({
-    mode: 'history',
-    routes
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
